adbe51aed335ed80da3eb135af3541afbc86c174
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2023-11-03 22:22:27 +0100

    Changed route from Product to Products, also Nav Changes

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -15,35 +15,45 @@ import {
 } from "@/components/ui/navigation-menu";
 
 const NavBar = () => {
-
   return (
     <nav className="flex justify-end md:flex-row md:justify-center">
-      {/* Desktop */}
-      <NavigationMenu>
+      <NavigationMenu className="hidden md:block">
         <NavigationMenuList>
-          {navbarlinks.map((navLink) => (
-            <NavigationMenuItem key={navLink.id}>
-              <Link href={navLink.href} passHref>
-                <NavigationMenuLink>{navLink.title}</NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          ))}
-          {navbardownlinks.map((navDownLinks) => (
-            <NavigationMenuItem key={navDownLinks.id}>
-              <NavigationMenuTrigger>{navDownLinks.title}</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul>
-                  {navDownLinks.sub.map((subLink) => (
-                    <li key={subLink.id}>
-                      <Link href={subLink.href} passHref>
-                        <NavigationMenuLink>{subLink.title}</NavigationMenuLink>
-                      </Link>
+          <NavigationMenuItem>
+            {navbardownlinks.map((navLink) => (
+              <NavigationMenuTrigger key={navLink.id}>
+                {navLink.title}
+              </NavigationMenuTrigger>
+            ))}
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                {navbardownlinks.map((navLink) =>
+                  navLink.sub.map((subLink) => (
+                    <li className="row-span-3" key={subLink.id}>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                        href={subLink.href}
+                      >
+                        {subLink.title}
+                      </NavigationMenuLink>
                     </li>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          ))}
+                  ))
+                )}
+              </ul>
+            </NavigationMenuContent>
+            {navbarlinks.map((navLink) => (
+              <Link
+                key={navLink.id}
+                href={navLink.href}
+                legacyBehavior
+                passHref
+              >
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {navLink.title}
+                </NavigationMenuLink>
+              </Link>
+            ))}
+          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
     </nav>
