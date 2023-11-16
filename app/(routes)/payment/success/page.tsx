21c785006f3eff37d9739eeb7de9d024e3d3a7fc
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2023-11-16 11:22:48 +0100

    ""

--- app/(routes)/payment/success/page.tsx
+++ app/(routes)/payment/success/page.tsx
@@ -9,9 +9,9 @@ const StripeSuccess = () => {
       <div className="mt-32 md:max-w-[50vw] mx-auto">
         <CheckCheck className="text-green-600 w-16 h-16 mx-auto my-6" />
         <div className="text-center">
-        <h1 className="text-center mt-10 text-3xl text-red-900">Payment Done!</h1>
-          <p className="my-2">Thank you for your purchase! We hope you enjoy it</p>
-          <p>Have a great day!</p>
+        <h1 className="text-center mt-10 text-3xl text-red-900">Payment Done! (Not Really)</h1>
+          <p className="my-2">This website is under build</p>
+          <p>No payment have been done</p>
           <Button className="mt-4"><Link href="/">GO Back</Link></Button>
         </div>
       </div>
