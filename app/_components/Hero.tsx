fe4331919e9f77c8edf1f021153ae0761b24dc66
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2023-11-26 15:21:09 +0100

    Made Hero SSR

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,17 +1,11 @@
-"use client"
-
 import React from "react";
 import Image from "next/image";
-import { useTheme } from "next-themes";
 import logo from "/public/hero/logo.gif";
 
 const Hero = () => {
-  const { theme, setTheme } = useTheme();
 
   return (
-    <div>
-      {theme === "light" ? (
-      <Image 
+      <Image
         src={logo}
         alt=""
         width={900}
@@ -19,17 +13,6 @@ const Hero = () => {
         className="px-10 mt-10"
         priority={true}
       />
-      ) : (
-         <Image 
-        src={logo}
-        alt=""
-        width={900}
-        height={900}
-        className="px-10 mt-10"
-        priority={true}
-      />
-      )}
-    </div>
   );
 };
 
